d827cd96724f67deb5a9896ac56cbaf64bf1e6ef
Nirmitha-J <77011640+Nirmitha-J@users.noreply.github.com>
2021-04-04 08:48:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+setTimeout(() => {
+    var para1 = document.createElement("p");
+    para1.innerText = '10';
+    var element = document.getElementById("div1");
+    element.appendChild(para1);
+
+
+    setTimeout(() => {
+        var para1 = document.createElement("p");
+        para1.innerText = '9';
+        var element = document.getElementById("div1");
+        element.appendChild(para1);
+
+        setTimeout(() => {
+            var para2 = document.createElement("p");
+            para2.innerText = '8';
+            var element = document.getElementById("div1");
+            element.appendChild(para2);
+
+            setTimeout(() => {
+                var para3 = document.createElement("p");
+                para3.innerText = '7';
+                var element = document.getElementById("div1");
+                element.appendChild(para3);
+
+                setTimeout(() => {
+                    var para4 = document.createElement("p");
+                    para4.innerText = '6';
+                    var element = document.getElementById("div1");
+                    element.appendChild(para4);
+
+                    setTimeout(() => {
+                        var para5 = document.createElement("p");
+                        para5.innerText = '5';
+                        var element = document.getElementById("div1");
+                        element.appendChild(para5);
+
+                        setTimeout(() => {
+                            var para6 = document.createElement("p");
+                            para6.innerText = '4';
+                            var element = document.getElementById("div1");
+                            element.appendChild(para6);
+
+                            setTimeout(() => {
+                                var para7 = document.createElement("p");
+                                para7.innerText = '3';
+                                var element = document.getElementById("div1");
+                                element.appendChild(para7);
+
+                                setTimeout(() => {
+                                    var para8 = document.createElement("p");
+                                    para8.innerText = '2';
+                                    var element = document.getElementById("div1");
+                                    element.appendChild(para8);
+
+                                    setTimeout(() => {
+                                        var para9 = document.createElement("p");
+                                        para9.innerText = '1';
+                                        var element = document.getElementById("div1");
+                                        element.appendChild(para9);
+                                    }, 500)
+                                    setTimeout(() => {
+                                        var h = document.createElement("h1");
+                                        h.innerText = 'Happy Independence Day!!';
+                                        var element = document.getElementById("div1");
+                                        element.appendChild(h);
+
+                                    }, 500)
+                                }, 500)
+                            }, 500)
+                        }, 500)
+                    }, 500)
+                }, 500)
+            }, 500)
+        }, 500)
+    }, 500)
+}, 500)
